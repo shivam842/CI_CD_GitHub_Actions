2a4d48efebc6e6bc374e9b9db3da0aac06f980fb
Shivam <shivampawar842@gmail.com>
2024-03-21 00:50:13 +0530

    first commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CI_CD_GitHub_Actions"
+include(":app")
